c1924cea87c923392687e3ca4938ba89fcd5a366
Robert Kleemann <robert@kleemann.org>
2015-03-04 17:42:27 -0800

    enable success/failure

--- src/main/scala/org/kleemann/rftg_flash_cards/Main.scala
+++ src/main/scala/org/kleemann/rftg_flash_cards/Main.scala
@@ -2,6 +2,9 @@ package org.kleemann.rftg_flash_cards
 
 import org.scaloid.common._
 
+// for 'sp' and other postfix ops
+import scala.language.postfixOps
+
 import java.io.File
 import java.text.SimpleDateFormat
 import java.util.Date
@@ -17,6 +20,9 @@ import android.widget.{FrameLayout, Toast}
 
 class Main extends SActivity  {
 
+  var successButton: SButton = null
+  var failButton: SButton = null
+
   onCreate {
     contentView = new SFrameLayout {
 
@@ -24,18 +30,38 @@ class Main extends SActivity  {
 
         this += new SLinearLayout {
           STextView("12/55").wrap
-          SButton("Restart").wrap.onClick { toast("Restart") }
+          SButton("Restart").wrap.onClick {
+            toast("Restart")
+            setTileSelected(false)
+          }
         }
 
-        SButton("Tile").textSize(24.5 sp).onClick { toast("Tile") }
+        SButton("Tile").textSize(24.5 sp).onClick {
+          toast("Tile")
+          setTileSelected(true)
+        }
 
         this += new SLinearLayout {
-          SButton("Success").wrap.onClick { toast("Success") }
-          SButton("Failure").wrap.onClick { toast("Failure") }
+          successButton = new SButton("Success")
+          successButton.wrap.onClick { toast("Success") }
+          this += successButton
+
+          failButton = new SButton("Fail")
+          failButton.wrap.onClick { toast("Fail") }
+          this += failButton
+
         }
       }
 
     } padding 20.dip
+
+    setTileSelected(false)
   }
 
+  var isTileSelected: Boolean = false
+  def setTileSelected(b: Boolean) {
+    isTileSelected = b
+    successButton.enabled(isTileSelected)
+    failButton.enabled(isTileSelected)
+  }
 }
